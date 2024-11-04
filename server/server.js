c8a85de7d9b5fa01853a50a8e1664d1822f052fa
Fardin2112 <fardinalam2112@gmail.com>
2024-11-05 02:56:43 +0530

    mongodb connected "working on backend

---
+++ server/server.js
@@ -0,0 +1,26 @@
+import express from 'express'
+import cors from 'cors'
+import 'dotenv/config'
+import connectDB from './config/mongodb.js'
+
+
+// app config
+const app = express();
+const port = process.env.PORT || 4000
+connectDB()  // connection mongodb
+
+
+// middleware
+app.use(express.json())
+app.use(cors())
+
+// api endpoint
+
+
+app.get(('/'), (req,res)=> {
+    res.send('API WORKING')
+})
+
+app.listen(port,()=>{
+    console.log("Server is runnning at port",port)
+})
