e87a2ddf4d18ada0c80caf254dd0fa6632b85241
Fardin2112 <fardinalam2112@gmail.com>
2024-11-06 23:56:17 +0530

    update

---
+++ server/controllers/adminController.js
@@ -0,0 +1,14 @@
+
+
+
+
+// API for adding doctors
+const doctor = async() => {
+
+    try {
+        const {name, email, password, speciality, degree, experiance, about, fees, address} = req.body
+    } catch (error) {
+        
+    }
+
+}
